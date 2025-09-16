bb4a308cecf2790e3d242ef95616c2be9e0f50c8
honzikschenk <nojschenk@gmail.com>
2025-09-15 20:31:57 -0400

    Adds google calendar

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -13,7 +13,14 @@ export default async function Events() {
         </Link>
       </nav>
       <h1 className="text-4xl font-bold mb-8">Upcoming Events</h1>
-      <div className="p-8 text-red-600">TODO: INSERT GOOGLE CALENDAR</div>
+      <div className="p-8 text-red-600">
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=71730c85c9c972cf042377ab3f0508ae116d9dfcb84dad47b8df05b023dc04e4%40group.calendar.google.com&ctz=America%2FToronto"
+          style={{ border: 0 }}
+          width="100%"
+          height="600"
+        ></iframe>
+      </div>
     </div>
   );
 }
